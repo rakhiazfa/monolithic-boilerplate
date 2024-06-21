3650c2c4534720a693a82219f6a048004efcda55
rakhi <rakhiazfa0421@gmail.com>
2024-06-21 15:25:01 +0700

    install redux-ui

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -1,13 +1,17 @@
 'use client';
 
+import '@radix-ui/themes/styles.css';
 import store from '@/store';
 import { NextUIProvider } from '@nextui-org/react';
 import { Provider } from 'react-redux';
+import { Theme } from '@radix-ui/themes';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <Provider store={store}>
-            <NextUIProvider>{children}</NextUIProvider>
+            <NextUIProvider>
+                <Theme>{children}</Theme>
+            </NextUIProvider>
         </Provider>
     );
 };
