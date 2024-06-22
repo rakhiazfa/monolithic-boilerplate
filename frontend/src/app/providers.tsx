b00cb926e1e432097f60eed4cda2e17a3f55d9d1
rakhi <rakhiazfa0421@gmail.com>
2024-06-22 15:23:34 +0700

    remove next-ui/react

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -2,16 +2,16 @@
 
 import '@radix-ui/themes/styles.css';
 import store from '@/store';
-import { NextUIProvider } from '@nextui-org/react';
 import { Provider } from 'react-redux';
 import { Theme } from '@radix-ui/themes';
+import { NextUIProvider } from '@nextui-org/system';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
         <Provider store={store}>
-            <NextUIProvider>
-                <Theme className="!bg-light">{children}</Theme>
-            </NextUIProvider>
+            <Theme className="!bg-light">
+                <NextUIProvider>{children}</NextUIProvider>
+            </Theme>
         </Provider>
     );
 };
